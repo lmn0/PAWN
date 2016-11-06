064b96e814c528629cef8cbb921cabc9fbac8023
Thejass Krishnan <tjskrishna@gmail.com>
2016-11-06 14:57:31 -0800

    Changed function

--- compose/recieve.js
+++ compose/recieve.js
@@ -3,12 +3,11 @@ var q;
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     q = 'hello';
-
     ch.assertQueue(q, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+	ch.consume(q, function(msg) {
+  		console.log(" [x] Received %s", msg.content.toString());
+	}, {noAck: true});
   });
 });
 
-console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-ch.consume(q, function(msg) {
-  console.log(" [x] Received %s", msg.content.toString());
-}, {noAck: true});
