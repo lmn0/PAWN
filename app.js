a73072e31bf58c472a53b29609c35b304b4ce8f1
Thejass Krishnan <tjskrishna@gmail.com>
2016-11-06 19:17:11 -0800

    Minor Changes

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 var express = require('express');
 var path = require('path');
-
 var stormpath =require('express-stormpath');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index.js');
-var users = require('./routes/users.js');
-
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//MIDDLEWARE
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Methods", ['GET','DELETE','PUT', 'POST']);
+			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			return next();
+		});
 
-app.use('/', index);
-app.use('/users', users);
+
+//ROUTE SETUP
+//Static
+app.use(express.static(path.join(__dirname, 'public')));
+//Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
