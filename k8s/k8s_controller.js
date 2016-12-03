26708f5d5e3740f7ac0b7a2f22aa557edbabc359
Thejass Krishnan <tjskrishna@gmail.com>
2016-12-02 21:39:16 -0800

    changes to k8s controller

--- k8s/k8s_controller.js
+++ k8s/k8s_controller.js
@@ -2,13 +2,36 @@ var amqp=require('amqplib/callback_api')
 
 var amqpURL="amqp://yqkistry:l84R0zn4Dl6R-9c1PrDfVhFr9pEc4DZN@hyena.rmq.cloudamqp.com/yqkistry"
 
+var fs = require('fs');
+var dir = './tmp';
+var jsonfile = require('jsonfile');
+
+
 amqp.connect(amqpURL,function(err,conn){
   conn.createChannel(function(err,ch){
     var q ='k8dcontroller'
     ch.assertQueue(q,{durable:false})
     console.log("<- wAiting for messages in %s",q)
     ch.consume(q,function(msg){
-      console.log("<- Recieved %s ",msg.content.toString());
+    
+    	var jsondata={};
+      	console.log("<- Recieved %s ",msg.content.toString());
+      	jsondata = JSON.parse(msg.content.toString());
+
+     	dir = dir + jsondata.projectID;
+     	if(!fs.existsSync(dir)){
+      		fs.mkdirSync(dir);
+     	}
+
+     	for(key in jsondata.config){
+      		jsonfile.writeFile(dir+'/'+key+'.json',jsondata.config[key],function(){
+      			console.err(err);
+      		})
+
+     	}
+
+
+
     },{noAck:true})
   })
 })
